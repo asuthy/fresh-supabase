4d45e8aa5a24f3a517c0348c714fb35e9104b73f
Andy Sutherland <asutherland@pcs-publishing.com>
2023-05-25 12:00:04 +0100

    Use env for redis

--- lib/redis.ts
+++ lib/redis.ts
@@ -1,12 +1,14 @@
 import { Redis } from "@upstash/redis";
 
-const REDIS_USER = Deno.env.get("REDIS_USER") || "";
-const REDIS_PASSWORD = Deno.env.get("REDIS_PASSWORD") || "";
-const REDIS_HOSTNAME = Deno.env.get("REDIS_HOSTNAME") || "";
-const REDIS_PORT = Deno.env.get("REDIS_PORT") || "";
+const REDIS_URL = Deno.env.get("REDIS_URL") || "";
+const REDIS_TOKEN = Deno.env.get("REDIS_TOKEN") || "";
+
+console.log({
+  url: REDIS_URL,
+  token: REDIS_TOKEN,
+});
 
 export const redis = await new Redis({
-  url: "https://eu1-choice-yak-39667.upstash.io",
-  token:
-    "AZrzASQgYzlhYjI5NTItMjA2Ni00ZGY3LThlNmQtNjNmYjcxNGFiMjU4YWMxMjE2ZjYwOTNlNGRlZDk4OWUyZTdkZmMwMzk5ZGY=",
+  url: REDIS_URL,
+  token: REDIS_TOKEN,
 });
